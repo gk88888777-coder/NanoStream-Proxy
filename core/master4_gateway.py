dac3c3cea507e4262ae6299636132dce5b2a31f4
gk88888777-coder <gk88888777@gmail.com>
2026-09-25 22:50:23 +0530

    Update master4_gateway.py

--- core/master4_gateway.py
+++ core/master4_gateway.py
@@ -31,7 +31,7 @@ logging.basicConfig(
 )
 
 BASE_URL = os.environ.get("BASE_URL", "https://nanostream4x.duckdns.org").rstrip('/')
-PROXY_PORT = int(os.environ.get("PROXY_PORT", 8080))
+PROXY_PORT = int(os.environ.get("PROXY_PORT", 8000))
 PROXY_DOMAIN = urlparse(BASE_URL).hostname or "nanostream4x.duckdns.org"
 
 SHARED_SSL_CONTEXT = ssl.create_default_context()
